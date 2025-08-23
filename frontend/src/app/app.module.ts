77e5fe906c2b1dd4fdccffbc440ffd6195bc1507
Matias Romero <matiassebrom@gmail.com>
2025-08-23 19:34:26 -0300

    se agrega angula material

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}

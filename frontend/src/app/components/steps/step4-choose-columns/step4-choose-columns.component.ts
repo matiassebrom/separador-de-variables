69f4dc7b2fef368626e437dee220f6461b18b509
Matias Romero <matiassebrom@gmail.com>
2025-09-11 09:11:58 -0300

    se implemento el paso 4 en un componente independiente

--- frontend/src/app/components/steps/step4-choose-columns/step4-choose-columns.component.ts
+++ frontend/src/app/components/steps/step4-choose-columns/step4-choose-columns.component.ts
@@ -7,34 +7,27 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-step4-choose-columns',
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    MatExpansionModule,
-    MatCheckboxModule,
-    MatButtonModule,
-    MatIconModule
-  ],
-  templateUrl: './step4-choose-columns.component.html',
-  styleUrl: './step4-choose-columns.component.scss'
+	selector: 'app-step4-choose-columns',
+	standalone: true,
+	imports: [CommonModule, FormsModule, MatExpansionModule, MatCheckboxModule, MatButtonModule, MatIconModule],
+	templateUrl: './step4-choose-columns.component.html',
+	styleUrl: './step4-choose-columns.component.scss'
 })
 export class Step4ChooseColumnsComponent {
-  @Input() isStepCurrent = false;
-  @Input() canAccessStep = false;
-  @Input() isStepCompleted = false;
-  @Input() headers: string[] = [];
-  @Input() selectedColumnsMap: Record<string, boolean> = {};
-  @Input() selectedColumns: string[] = [];
-  @Output() columnCheckboxChange = new EventEmitter<{ header: string; checked: boolean }>();
-  @Output() nextStep = new EventEmitter<void>();
+	@Input() isStepCurrent = false;
+	@Input() canAccessStep = false;
+	@Input() isStepCompleted = false;
+	@Input() headers: string[] = [];
+	@Input() selectedColumnsMap: Record<string, boolean> = {};
+	@Input() selectedColumns: string[] = [];
+	@Output() columnCheckboxChange = new EventEmitter<{ header: string; checked: boolean }>();
+	@Output() nextStep = new EventEmitter<void>();
 
-  onCheckboxChange(header: string, checked: boolean) {
-    this.columnCheckboxChange.emit({ header, checked });
-  }
+	onCheckboxChange(header: string, checked: boolean) {
+		this.columnCheckboxChange.emit({ header, checked });
+	}
 
-  onContinue() {
-    this.nextStep.emit();
-  }
+	onContinue() {
+		this.nextStep.emit();
+	}
 }
